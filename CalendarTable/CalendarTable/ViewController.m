d2ea7583d23a9e67448e18b1035f0efb89037ce6
wxhl <wangch6688@hotmail.com>
2016-07-30 10:11:16 +0800

    add

--- CalendarTable/CalendarTable/ViewController.m
+++ CalendarTable/CalendarTable/ViewController.m
@@ -20,9 +20,7 @@
     
     
     [self _loadData];
-    
-    
-    
+
     
 }
 
@@ -34,17 +32,24 @@
     
     
     
+    
 }
 
 
 
 #pragma mark - 日期的获取
-//今天是哪一天
--(NSInteger)day:(NSDate * )date{
+//这个月总共多少天
+-(NSInteger)lenth:(NSDate * )date{
     
-    NSDateComponents * compoents = [[NSCalendar currentCalendar]components:(kCFCalendarUnitYear|kCFCalendarUnitMonth|kCFCalendarUnitDay) fromDate:date];
+    //    NSDateComponents * compoents = [[NSCalendar currentCalendar]components:(kCFCalendarUnitYear|kCFCalendarUnitMonth|kCFCalendarUnitDay) fromDate:date];
     
-    return [compoents day];
+    //获取每个月的天数范围
+    NSRange daysInLastMonth = [[NSCalendar currentCalendar]rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date];
+    
+    NSInteger days = daysInLastMonth.length;
+    
+    
+    return days;
 }
 
 //本月是一年中的第几个月
@@ -55,6 +60,8 @@
     return [compoents month];
 }
 
+
+
 //今年是什么年份
 -(NSInteger)year:(NSDate * )date{
     
@@ -63,18 +70,49 @@
     return [compoents year];
 }
 
-//这个月总共多少天
--(NSInteger)lenth:(NSDate * )date{
-    
-//    NSDateComponents * compoents = [[NSCalendar currentCalendar]components:(kCFCalendarUnitYear|kCFCalendarUnitMonth|kCFCalendarUnitDay) fromDate:date];
-    
-    //获取每个月的天数范围
-    NSRange daysInLastMonth = [[NSCalendar currentCalendar]rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date];
-    
-    NSInteger days = daysInLastMonth.length;
+
+//这个月的第一天是周几
+- (NSInteger)firstWeekDayInThisMonth:(NSDate *)date{
     
     
-    return days;
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    
+    [calendar setFirstWeekday:1];//1.Sun. 2.Mon. 3.Thes. 4.Wed. 5.Thur. 6.Fri. 7.Sat.
+    NSDateComponents *comp = [calendar components:(NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay) fromDate:date];
+    [comp setDay:1];
+    NSDate *firstDayOfMonthDate = [calendar dateFromComponents:comp];
+    
+    NSUInteger firstWeekday = [calendar ordinalityOfUnit:NSCalendarUnitWeekday inUnit:NSCalendarUnitWeekOfMonth forDate:firstDayOfMonthDate];
+    return firstWeekday - 1;
+
+    
+    
+}
+
+
+//今天是哪一天
+-(NSInteger)day:(NSDate * )date{
+    
+    NSDateComponents * compoents = [[NSCalendar currentCalendar]components:(kCFCalendarUnitYear|kCFCalendarUnitMonth|kCFCalendarUnitDay) fromDate:date];
+    
+    return [compoents day];
+}
+
+
+//上个月的的时间
+- (NSDate *)lastMonth:(NSDate *)date{
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.month = -1;
+    NSDate *newDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:date options:0];
+    return newDate;
+}
+
+//下一个月的时间
+- (NSDate*)nextMonth:(NSDate *)date{
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.month = +1;
+    NSDate *newDate = [[NSCalendar currentCalendar] dateByAddingComponents:dateComponents toDate:date options:0];
+    return newDate;
 }
 
 
